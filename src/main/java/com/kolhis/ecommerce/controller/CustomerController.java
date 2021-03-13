8cb16c842c799fef8ac5341e43ff6de58020e00a
OGUZ TOPAL <oguz.topal@consultant.turkcell.com.tr>
2021-03-13 21:45:33 +0300

    test

--- src/main/java/com/kolhis/ecommerce/controller/CustomerController.java
+++ src/main/java/com/kolhis/ecommerce/controller/CustomerController.java
@@ -5,6 +5,8 @@ import com.kolhis.ecommerce.entity.Customer;
 import com.kolhis.ecommerce.entity.dto.CustomerDto;
 import com.kolhis.ecommerce.service.AbstractEntityService;
 import com.kolhis.ecommerce.service.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(CustomerController.END_POINT)
+@Api(value = "Config Controller", description = "config ile alakali adresleri besler")
 public class CustomerController extends AbstractEntityController<CustomerDto, Customer, Long> {
 
     static final String END_POINT = "/api/customer";
@@ -22,11 +25,13 @@ public class CustomerController extends AbstractEntityController<CustomerDto, Cu
 
 
     @PostMapping("/save")
+    @ApiOperation(value = "refreshLog", notes = " Log seviyelerinde bir degisiklik oldgunda degisiklik gosterir")
     public CustomerDto saveCompany(@RequestBody CustomerDto dto) {
         return toDto(customerService.save(toEntity(dto)));
     }
 
     @Override
+    @ApiOperation(value = "refreshLog", notes = " Log seviyelerinde bir degisiklik oldgunda degisiklik gosterir")
     protected AbstractEntityService<Customer, Long> getService() {
         return this.customerService;
     }
--- src/main/java/com/kolhis/ecommerce/entity/dto/CustomerDto.java
+++ src/main/java/com/kolhis/ecommerce/entity/dto/CustomerDto.java
@@ -1,13 +1,18 @@
 package com.kolhis.ecommerce.entity.dto;
 
 import com.kolhis.ecommerce.repository.BaseDto;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @Data
 @ToString(callSuper = true)
+@ApiModel(value = "Company Data Transfer Object")
 public class CustomerDto extends BaseDto {
+    @ApiModelProperty(value = "First Name", required = true)
     private String firstName;
+    @ApiModelProperty(value = "Last Name", required = true)
     private String lastName;
 }
